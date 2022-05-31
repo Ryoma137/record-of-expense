aab0b48f3fbb18ea9cae1cb0688ea530612ed2e6
Ryoma Ishizaki <38678925+Ryoma137@users.noreply.github.com>
2022-06-01 02:27:34 +0900

    Use @Controller instead of @RestController so as to return html, not just String

--- src/main/java/com/recordofexpense/recordofexpense/controller/IncomeController.java
+++ src/main/java/com/recordofexpense/recordofexpense/controller/IncomeController.java
@@ -1,32 +1,22 @@
 package com.recordofexpense.recordofexpense.controller;
 
-import com.recordofexpense.recordofexpense.entity.Amount;
-import com.recordofexpense.recordofexpense.repository.AmountRepository;
-import lombok.AllArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.recordofexpense.recordofexpense.service.AmountService;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-import java.util.stream.StreamSupport;
 
-@AllArgsConstructor
-@RestController
+
+@Controller
 public class IncomeController {
 
-    @Autowired
-    private final AmountRepository amountRepository;
-
-    @GetMapping("/amount/list")
-    public List<Amount> getAmount() {
-        return StreamSupport.stream(amountRepository.findAll().spliterator(), false).toList();
-    }
+    private AmountService amountService;
 
     @GetMapping("/expense")
-    public String getCategory(Model model){
-        List<String> categoryList = StreamSupport.stream(amountRepository.getCategoryList().spliterator(),false).toList();
-        model.addAttribute("categoryLists",categoryList);
+    public String getCategory(Model model) {
+        List<String> categoryList = amountService.getCategoryList();
+        model.addAttribute("categoryLists", categoryList);
         return "expense";
     }
 }
