c6a3f585be2894d1a276248aae08f9f0788ab71d
Ryoma Ishizaki <38678925+Ryoma137@users.noreply.github.com>
2022-06-01 16:47:36 +0900

    Fix test cases based on new test data

--- src/test/java/com/recordofexpense/recordofexpense/service/AmountServiceTest.java
+++ src/test/java/com/recordofexpense/recordofexpense/service/AmountServiceTest.java
@@ -24,7 +24,7 @@ class AmountServiceTest {
     void testFindAll() {
         List<Amount> actual = amountService.getFindAll();
         assertNotNull(actual, "Amount should not null");
-        assertEquals(9, actual.size());
+        assertEquals(6, actual.size());
 
         assertEquals(1, actual.get(0).getId());
         assertEquals("Sandwich", actual.get(0).getName());
@@ -51,35 +51,16 @@ class AmountServiceTest {
         assertEquals("Thai Cuisine", actual.get(3).getComments());
 
         assertEquals(5, actual.get(4).getId());
-        assertEquals("Black T-Shirt", actual.get(4).getName());
-        assertEquals(1080, actual.get(4).getPrice());
-        assertEquals("Clothes", actual.get(4).getCategory());
-        assertEquals("H&M T-Shirt", actual.get(4).getComments());
+        assertEquals("iPad", actual.get(4).getName());
+        assertEquals(140000, actual.get(4).getPrice());
+        assertEquals("Gadget", actual.get(4).getCategory());
+        assertEquals("iPad Pro", actual.get(4).getComments());
 
         assertEquals(6, actual.get(5).getId());
-        assertEquals("iPad", actual.get(5).getName());
-        assertEquals(140000, actual.get(5).getPrice());
-        assertEquals("Gadget", actual.get(5).getCategory());
-        assertEquals("iPad Pro", actual.get(5).getComments());
-
-        assertEquals(7, actual.get(6).getId());
-        assertEquals("Crispy Pizza", actual.get(6).getName());
-        assertEquals(850, actual.get(6).getPrice());
-        assertEquals("Food", actual.get(6).getCategory());
-        assertEquals("gluten free", actual.get(6).getComments());
-
-        assertEquals(8, actual.get(7).getId());
-        assertEquals("Jeans", actual.get(7).getName());
-        assertEquals(12000, actual.get(7).getPrice());
-        assertEquals("Clothes", actual.get(7).getCategory());
-        assertEquals("Levis", actual.get(7).getComments());
-
-        assertEquals(9, actual.get(8).getId());
-        assertEquals("MacBook", actual.get(8).getName());
-        assertEquals(150000, actual.get(8).getPrice());
-        assertEquals("Gadget", actual.get(8).getCategory());
-        assertEquals("MacBook Air", actual.get(8).getComments());
-
+        assertEquals("Crispy Pizza", actual.get(5).getName());
+        assertEquals(850, actual.get(5).getPrice());
+        assertEquals("Food", actual.get(5).getCategory());
+        assertEquals("gluten free", actual.get(5).getComments());
     }
 
     @Test
@@ -89,15 +70,12 @@ class AmountServiceTest {
         List<String> actual = amountService.getCategoryList();
         assertEquals("Food", actual.get(0));
         assertEquals("Food", actual.get(3));
-        assertEquals("Food", actual.get(6));
+        assertEquals("Food", actual.get(5));
 
         assertEquals("Clothes", actual.get(1));
-        assertEquals("Clothes", actual.get(4));
-        assertEquals("Clothes", actual.get(7));
 
         assertEquals("Gadget", actual.get(2));
-        assertEquals("Gadget", actual.get(5));
-        assertEquals("Gadget", actual.get(8));
+        assertEquals("Gadget", actual.get(4));
 
     }
 }
