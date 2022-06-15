26fd45219e7a1533712acf81de76c66b54952eae
Ryoma Ishizaki <38678925+Ryoma137@users.noreply.github.com>
2022-06-15 12:40:13 +0900

    Add test of testUpdateWhenDataNotContainsSameKeyWithGivenData in AmountRepositoryTest

--- src/test/java/com/recordofexpense/recordofexpense/repository/AmountRepositoryTest.java
+++ src/test/java/com/recordofexpense/recordofexpense/repository/AmountRepositoryTest.java
@@ -9,8 +9,7 @@ import org.springframework.test.context.jdbc.Sql;
 
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class AmountRepositoryTest {
@@ -162,6 +161,95 @@ class AmountRepositoryTest {
     @DisplayName("与えられたデータのキーが一致するデータがDBに存在しない時、DBが更新されないこと")
     void testUpdateWhenDataNotContainsSameKeyWithGivenData() {
 
+
+        List<Amount> originalValues = amountRepository.findAll();
+
+        assertEquals(6, originalValues.size());
+
+        assertEquals(1, originalValues.get(0).getId());
+        assertEquals("Sandwich", originalValues.get(0).getName());
+        assertEquals(450, originalValues.get(0).getPrice());
+        assertEquals("Food", originalValues.get(0).getCategory());
+        assertEquals("What a scrumptious sandwich", originalValues.get(0).getComments());
+
+        assertEquals(2, originalValues.get(1).getId());
+        assertEquals("White T-Shirt", originalValues.get(1).getName());
+        assertEquals(980, originalValues.get(1).getPrice());
+        assertEquals("Clothes", originalValues.get(1).getCategory());
+        assertEquals("Uniqlo T-Shirt", originalValues.get(1).getComments());
+
+        assertEquals(3, originalValues.get(2).getId());
+        assertEquals("iPhone", originalValues.get(2).getName());
+        assertEquals(130000, originalValues.get(2).getPrice());
+        assertEquals("Gadget", originalValues.get(2).getCategory());
+        assertEquals("iPhone 13 Pro", originalValues.get(2).getComments());
+
+        assertEquals(4, originalValues.get(3).getId());
+        assertEquals("Green Curry", originalValues.get(3).getName());
+        assertEquals(880, originalValues.get(3).getPrice());
+        assertEquals("Food", originalValues.get(3).getCategory());
+        assertEquals("Thai Cuisine", originalValues.get(3).getComments());
+
+        assertEquals(5, originalValues.get(4).getId());
+        assertEquals("iPad", originalValues.get(4).getName());
+        assertEquals(140000, originalValues.get(4).getPrice());
+        assertEquals("Gadget", originalValues.get(4).getCategory());
+        assertEquals("iPad Pro", originalValues.get(4).getComments());
+
+        assertEquals(6, originalValues.get(5).getId());
+        assertEquals("Crispy Pizza", originalValues.get(5).getName());
+        assertEquals(850, originalValues.get(5).getPrice());
+        assertEquals("Food", originalValues.get(5).getCategory());
+        assertEquals("gluten free", originalValues.get(5).getComments());
+
+        var amount = new Amount();
+        amount.setId(7L);
+        amount.setName("testName");
+        amount.setPrice(1000);
+        amount.setCategory("testCategory");
+        amount.setComments("testComment");
+
+        amountRepository.save(amount);
+        List<Amount> updatedValues = amountRepository.findAll();
+
+
+        assertEquals(1, updatedValues.get(0).getId(), "DBのID列1列目に保存されているデータのIDが更新されていないかの確認");
+        assertEquals("Sandwich", updatedValues.get(0).getName(), "DBのID列1列目に保存されているデータの名前が更新されていないかの確認");
+        assertEquals(450, updatedValues.get(0).getPrice(), "DBのID列1列目に保存されているデータの価格が更新されていないかの確認");
+        assertEquals("Food", updatedValues.get(0).getCategory(), "DBのID列1列目に保存されているデータのカテゴリーが更新されていないかの確認");
+        assertEquals("What a scrumptious sandwich", updatedValues.get(0).getComments(), "DBのID列1列目に保存されているデータのコメントが更新されていないかの確認");
+
+        assertEquals(2, updatedValues.get(1).getId(), "DBのID列2列目に保存されているデータのIDが更新されていないかの確認");
+        assertEquals("White T-Shirt", updatedValues.get(1).getName(), "DBのID列2列目に保存されているデータの名前が更新されていないかの確認");
+        assertEquals(980, updatedValues.get(1).getPrice(), "DBのID列2列目に保存されているデータの価格が更新されていないかの確認");
+        assertEquals("Clothes", updatedValues.get(1).getCategory(), "DBのID列2列目に保存されているデータのカテゴリーが更新されていないかの確認");
+        assertEquals("Uniqlo T-Shirt", updatedValues.get(1).getComments(), "DBのID列2列目に保存されているデータのコメントが更新されていないかの確認");
+
+        assertEquals(3, updatedValues.get(2).getId(), "DBのID列3列目に保存されているデータのIDが更新されていないかの確認");
+        assertEquals("iPhone", updatedValues.get(2).getName(), "DBのID列3列目に保存されているデータの名前が更新されていないかの確認");
+        assertEquals(130000, updatedValues.get(2).getPrice(), "DBのID列3列目に保存されているデータの価格が更新されていないかの確認");
+        assertEquals("Gadget", updatedValues.get(2).getCategory(), "DBのID列3列目に保存されているデータのカテゴリーが更新されていないかの確認");
+        assertEquals("iPhone 13 Pro", updatedValues.get(2).getComments(), "DBのID列3列目に保存されているデータのコメントが更新されていないかの確認");
+
+        assertEquals(4, updatedValues.get(3).getId(), "DBのID列4列目に保存されているデータのIDが更新されていないかの確認");
+        assertEquals("Green Curry", updatedValues.get(3).getName(), "DBのID列4列目に保存されているデータの名前が更新されていないかの確認");
+        assertEquals(880, updatedValues.get(3).getPrice(), "DBのID列4列目に保存されているデータの価格が更新されていないかの確認");
+        assertEquals("Food", updatedValues.get(3).getCategory(), "DBのID列4列目に保存されているデータのカテゴリーが更新されていないかの確認");
+        assertEquals("Thai Cuisine", updatedValues.get(3).getComments(), "DBのID列4列目に保存されているデータのコメントが更新されていないかの確認");
+
+        assertEquals(5, updatedValues.get(4).getId(), "DBのID列5列目に保存されているデータのIDが更新されていないかの確認");
+        assertEquals("iPad", updatedValues.get(4).getName(), "DBのID列5列目に保存されているデータの名前が更新されていないかの確認");
+        assertEquals(140000, updatedValues.get(4).getPrice(), "DBのID列5列目に保存されているデータの価格が更新されていないかの確認");
+        assertEquals("Gadget", updatedValues.get(4).getCategory(), "DBのID列5列目に保存されているデータのカテゴリーが更新されていないかの確認");
+        assertEquals("iPad Pro", updatedValues.get(4).getComments(), "DBのID列5列目に保存されているデータのコメントが更新されていないかの確認");
+
+        assertEquals(6, updatedValues.get(5).getId(), "DBのID列6列目に保存されているデータのIDが更新されていないかの確認");
+        assertEquals("Crispy Pizza", updatedValues.get(5).getName(), "DBのID列6列目に保存されているデータの名前が更新されていないかの確認");
+        assertEquals(850, updatedValues.get(5).getPrice(), "DBのID列6列目に保存されているデータの価格が更新されていないかの確認");
+        assertEquals("Food", updatedValues.get(5).getCategory(), "DBのID列6列目に保存されているデータのカテゴリーが更新されていないかの確認");
+        assertEquals("gluten free", updatedValues.get(5).getComments(), "DBのID列6列目に保存されているデータのコメントが更新されていないかの確認");
+
+        assertEquals(amount, updatedValues.get(6), "与えられたデータのキーが一致するデータがDBに存在しない時、与えられたデータでDBの更新ではなく、与えられたデータのキーで新たなデータが登録されているかの確認");
     }
 
     //    @Test
